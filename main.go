0fddf1cbd5ce3211f30d7862e0d43c87d79a81da
Eric Christensen <hebrew.8.12@gmail.com>
2022-01-08 14:37:59 -0800

    init commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+
+	app.Listen(fmt.Sprintf(":%s", os.Getenv("PORT")))
+}
